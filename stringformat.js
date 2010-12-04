fa03911bbcb50f6fa7e946311386dc3ed57ebe08
Andreas Grech <agnt666@gmail.com>
2010-12-04 22:10:22 +0100

    added support for alignment

--- stringformat.js
+++ stringformat.js
@@ -1,5 +1,5 @@
 String.format = String.format || function (format) {
-	var params = [].splice.call(arguments, 1), nextOpen, nextClose = 0, index, plValue, spaces;
+	var params = [].splice.call(arguments, 1), nextOpen, nextClose = 0, index, plValue, spaces, totalSpaces;
 
 	while ((nextOpen = format.indexOf('{', nextClose)) >= 0) {
 		if (isNaN(+format[nextOpen+1])) {
@@ -8,13 +8,14 @@ String.format = String.format || function (format) {
 		}
 		nextClose = format.indexOf('}', nextOpen);
 		index = format.substring(nextOpen + 1, nextClose);
-		if (index.indexOf(',')) {
+		if (index.indexOf(',') > 0) {
 			spaces = +index.substring(index.indexOf(',') + 1);
 			index = +index.substring(0, index.indexOf(','));
 		}
 		plValue = params[index];
-		if ((spaces -= plValue.length) > 0) {
-			plValue += new Array(spaces + 1).join(" "); 
+		if (spaces) {
+			totalSpaces = new Array(Math.abs(spaces) - plValue.length + 1).join(" ");
+			plValue = spaces > 0 ? totalSpaces + plValue : plValue + totalSpaces;
 		}
 		format = format.substring(0, nextOpen) + ((!plValue && plValue != 0) ? "" : plValue) + format.substring(nextClose + 1);
 		spaces = 0;
