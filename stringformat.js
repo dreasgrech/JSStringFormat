ef0cd7a4f94b734dbbef23d72b0fe9961febd510
Andreas Grech <agnt666@gmail.com>
2012-10-21 02:16:45 +0200

    Fixing issue #1
    Expected behavior:
    String.format("{{0}}", "abc") => {0}
    String.format("{{{0}}}", "abc") => {abc}

--- stringformat.js
+++ stringformat.js
@@ -1,24 +1,34 @@
 String.format = String.format || function (format) {
-	var params = [].splice.call(arguments, 1), nextOpen, nextClose = 0, index, plValue, spaces, totalSpaces;
+    var params = [].splice.call(arguments, 1), nextOpen, nextClose = 0, index, plValue, spaces, totalSpaces,
+        removeCharAt = function (text, pos) {
+            return text.substr(0, pos) + text.substr(pos + 1, text.length);
+        };
 
-	while ((nextOpen = format.indexOf('{', nextClose)) >= 0) {
-		if (isNaN(+format[nextOpen+1])) {
-			nextClose = nextOpen + 1;
-			continue;
-		}
-		nextClose = format.indexOf('}', nextOpen);
-		index = format.substring(nextOpen + 1, nextClose);
-		if (index.indexOf(',') > 0) {
-			spaces = +index.substring(index.indexOf(',') + 1);
-			index = +index.substring(0, index.indexOf(','));
-		}
-		plValue = params[index] + '';
-		if (spaces) {
-			totalSpaces = new Array(Math.abs(spaces) - plValue.length + 1).join(" ");
-			plValue = spaces > 0 ? totalSpaces + plValue : plValue + totalSpaces;
-		}
-		format = format.substring(0, nextOpen) + ((!plValue && plValue != 0) ? "" : plValue) + format.substring(nextClose + 1);
-		spaces = 0;
-	}
-	return format;
+    while ((nextOpen = format.indexOf('{', nextClose)) >= 0) {
+        if (isNaN(+format[nextOpen+1])) {
+            if (format[nextOpen+1] === "{") {
+                format = removeCharAt(format, nextOpen + 1);
+                format = removeCharAt(format, format.indexOf('}', nextOpen-1));
+                nextClose++;
+                continue;
+            }
+            nextClose = nextOpen + 1;
+            continue;
+        }
+        
+        nextClose = format.indexOf('}', nextOpen);
+        index = format.substring(nextOpen + 1, nextClose);
+        if (index.indexOf(',') > 0) {
+            spaces = +index.substring(index.indexOf(',') + 1);
+            index = +index.substring(0, index.indexOf(','));
+        }
+        plValue = params[index] + '';
+        if (spaces) {
+            totalSpaces = new Array(Math.abs(spaces) - plValue.length + 1).join(" ");
+            plValue = spaces > 0 ? totalSpaces + plValue : plValue + totalSpaces;
+        }
+        format = format.substring(0, nextOpen) + ((!plValue && plValue != 0) ? "" : plValue) + format.substring(nextClose + 1);
+        spaces = 0;
+    }
+    return format;
 };
